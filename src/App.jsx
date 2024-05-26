304a86483f438e7648abaee5b6ef867d163ce7d3
Rakibul Islam <rakib.ca@gmail.com>
2024-05-26 11:13:04 -0600

    array created & used map to iterate

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
 import Avatar from "./Avatar";
 
+const data = [
+  {
+    id: "A",
+    src: "https://sandpack-bundler.vercel.app/img/avatars/001.png",
+    alt: "person with curly hair and a black T-shirt",
+  },
+  {
+    id: "B",
+    src: "https://sandpack-bundler.vercel.app/img/avatars/002.png",
+    alt: "person wearing a hijab and glasses",
+  },
+  {
+    id: "C",
+    src: "https://sandpack-bundler.vercel.app/img/avatars/003.png",
+    alt: "person with short hair wearing a blue hoodie",
+  },
+  {
+    id: "D",
+    src: "https://sandpack-bundler.vercel.app/img/avatars/004.png",
+    alt: "person with a pink mohawk and a raised eyebrow",
+  },
+];
+
 function App() {
   return (
     <div className="avatar-set">
-      <Avatar
-        src="https://sandpack-bundler.vercel.app/img/avatars/001.png"
-        alt="person with curly hair and a black T-shirt"
-      />
-      <Avatar
-        src="https://sandpack-bundler.vercel.app/img/avatars/002.png"
-        alt="person wearing a hijab and glasses"
-      />
-      <Avatar
-        src="https://sandpack-bundler.vercel.app/img/avatars/003.png"
-        alt="person with short hair wearing a blue hoodie"
-      />
-      <Avatar
-        src="https://sandpack-bundler.vercel.app/img/avatars/004.png"
-        alt="person with a pink mohawk and a raised eyebrow"
-      />
+      {data.map((avatarpic) => (
+        <Avatar key={avatarpic.id} src={avatarpic.src} alt={avatarpic.alt} />
+      ))}
     </div>
   );
 }
